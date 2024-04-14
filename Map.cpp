331cd401d02bb9d468da53223344f2e9e78c5a19
tpespos <tpespos@gmail.com>
2024-04-13 23:04:07 -0700

    Add project files.

---
+++ Map.cpp
@@ -0,0 +1,16 @@
+#include "Map.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+/*
+void Map::Test()
+{
+	cout << "/nPotato/n";
+}
+*/
+
+void Map::this_is_DUMB()
+{
+	cout << "\nPotatos\n";
+}
---
+++ Reduce.cpp
@@ -0,0 +1,1 @@
+#include "Reduce.h"
