b1a1ced1d11e6dfd9489c50beb6f99911e0caa2c
rlanders7 <landersryan27@gmail.com>
2024-04-14 18:37:29 -0700

    Adding gets/sets for file managment

--- File_Management.h
+++ File_Management.h
@@ -1,5 +1,36 @@
 #pragma once
+
+#include <string>
+using std::namespace;
+
 class File_Management
 {
+
+public:
+	int setPath(string input);
+
+	int setPlayFilename(string input);
+
+	int setTempFilename(string input);
+
+	string getPath();
+
+	string getPlayFilename();
+
+	string getTempFilename();
+
+
+private:
+
+	//main play text path
+	string path;
+	
+	//main play filename
+	string playFilaname;
+	
+	//temp file for post-map transfer to reduce class
+	string tempFilename;
+
+	
 };
 
