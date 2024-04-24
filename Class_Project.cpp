488d93c69c0e830bbb68913efaca8a5fa428befd
Cliff Hightower <cliffhightower@hotmail.com>
2024-04-23 19:32:07 -0700

    Created workflow function and tested currently working parts

--- Class_Project.cpp
+++ Class_Project.cpp
@@ -13,45 +13,64 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+int workflow();
 
 int main()
 {
         
+    int check = workflow();
+    if (check)
+    {
+        std::cout << "Failure in workflow.\n";
+        return 1;
+    }
+
+    std::cout << "End Program" << std::endl;
+
+}
+
+int workflow()
+{
+
     File_Management my_File_Management;
     Reduce myReducer;
 
-    //// Sample text file would look like this        a the is the a
-    //// This section happens outside of loop that iterates over all files in the input directory
-    my_File_Management.promptUserForDirectories();
+    // Commenting out for testing
+    //my_File_Management.promptUserForDirectories();
+    //vector<string> inputFiles = my_File_Management.getInputFiles();
+
+    // hardcodeing for testing (change to your local file location to quick testing)
+    string filepath1 = "C:\\Users\\lodin\\Documents\\College_work\\Spring2024\\OOD_Wensday_Class\\Project1\\shakespeare";
+    string filepath2 = "C:\\Users\\lodin\\Documents\\College_work\\Spring2024\\OOD_Wensday_Class\\Project1\\interFileLoc";
+    my_File_Management.setInputFileLocation(filepath1);
+    my_File_Management.setIntermediateFileLocation(filepath2);
+
     vector<string> inputFiles = my_File_Management.getInputFiles();
 
-    //// this line needs to be the first line in the loop that iterates over all files in the input directory
-    my_File_Management.setFileBeingWorked(inputFiles[0]);
+    // loop though each input file
+    for (int i = 0; i < my_File_Management.getNumberOfInputFiles(); i++)
+    {
+        my_File_Management.setFileBeingWorked(inputFiles[i]);
+
+        // Map
+        Map_Tokenizer mapTokenizerObj(my_File_Management);
+        mapTokenizerObj.parseLines();
+
+        // Sort
 
 
-    //****************
-    // This part happens inside of the Map Class
-    //my_File_Management.setFileBeingWorked("inputFile1.txt");
-    //Map_Tokenizer mapTokenizerObj(my_File_Management);
-    
-    //vector<string> Test_Map_Input_Vector = my_File_Management.importMapFile();
+        // Reduce
 
-    //string currentLine = " ode to my poor coding skills! Time to cry...";
 
-    //mapTokenizerObj.map(my_File_Management.getFileBeingWorked(), currentLine);
+        std::cout << "Loop " << i << " complete.\n";
 
-    vector<string> Test_Map_Input_Vector = my_File_Management.importMapFile();
-    // DO WORK
-    vector<string> Test_Map_Output_Vector = { "(“a”, 1)", "(“the”, 1)", "(“is”, 1)", "(“the”, 1)", "(“a”, 1)" };
-    my_File_Management.exportMapFile(Test_Map_Output_Vector);
-    //****************
-
+    }
 
     //################
     // This part happens inside of the Sort Class
     //vector<string> Test_Sort_Input_Vector = my_File_Management.importSortFile();
     //// DO WORK
-    Sort test_sort_class(my_File_Management);
+    //Sort test_sort_class(my_File_Management);
     //vector<string> Test_Sort_Output_Vector = { "(“a”,[1, 1, 1])", "(“the”,[1, 1])", "(“is”,[1])" };
     //my_File_Management.exportSortFile(Test_Sort_Output_Vector);
     //################
@@ -61,10 +80,13 @@ int main()
     // This part happens inside of the Reduce Class
     //vector<string> Test_Reduce_Input_Vector = my_File_Management.importReduceFile();
     // DO WORK
-    myReducer.fileManagerUpdate(my_File_Management);
-    myReducer.reduceWrapper();
-    
+    //myReducer.fileManagerUpdate(my_File_Management);
+    //myReducer.reduceWrapper();
+
     //vector<string> Test_Reduce_Output_Vector = { "(“a”,3)", "(“the”,2)", "(“is”,1)" };
     //my_File_Management.exportReduceFile(Test_Reduce_Output_Vector);
     //@@@@@@@@@@@@@@@@
+
+    return 0;
+
 }
