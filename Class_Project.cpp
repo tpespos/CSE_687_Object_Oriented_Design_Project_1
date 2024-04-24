74ebde0edc895bc632246335184cffa4737bac80
Cliff Hightower <cliffhightower@hotmail.com>
2024-04-24 00:34:31 -0700

    modified workflow

--- Class_Project.cpp
+++ Class_Project.cpp
@@ -13,37 +13,41 @@
 #include <iostream>
 #include <ctime>
 
-// Globals
-clock_t start_time; // Global for tic-toc functions
-char cwd[4096]; // Global for get current Dir
-
-// Function Definitions
-
-void tic(void) {
-
-    start_time = clock();
-
-}
-
-void toc() {
-
-    clock_t t = clock();
-    t = t - start_time;
-    double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
-    printf("This code block took %f seconds to execute \n", time_taken);
-
-}
-
+//// Globals
+//clock_t start_time; // Global for tic-toc functions
+//char cwd[4096]; // Global for get current Dir
+//
+//// Function Definitions
+//
+//void tic(void) {
+//
+//    start_time = clock();
+//
+//}
+//
+//void toc() {
+//
+//    clock_t t = clock();
+//    t = t - start_time;
+//    double time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
+//    printf("This code block took %f seconds to execute \n", time_taken);
+//
+//}
+//
 
 using namespace std;
 namespace fs = std::filesystem;
 
-int workflow();
+int workflow(File_Management my_File_Management);
 
 int main()
 {
-        
-    int check = workflow();
+    File_Management my_File_Management;
+    Reduce myReducer;
+
+    my_File_Management.promptUserForDirectories();
+    
+    int check = workflow(my_File_Management);
     if (check)
     {
         std::cout << "Failure in workflow.\n";
@@ -54,26 +58,9 @@ int main()
 
 }
 
-int workflow()
+int workflow(File_Management my_File_Management)
 {
-
-    File_Management my_File_Management;
-    Reduce myReducer;
-
-    // Commenting out for testing
-    my_File_Management.promptUserForDirectories();
     vector<string> inputFiles = my_File_Management.getInputFiles();
-
-    // hardcodeing for testing (change to your local file location to quick testing)
-    /*
-    string filepath1 = "C:\\Users\\lande\\OneDrive\\Documents\\!Masters\\shakespeare";
-    string filepath2 = "C:\\Users\\lande\\OneDrive\\Documents\\!Masters\\shakespeare\\interm";
-    string filepath3 = "C:\\Users\\lande\\OneDrive\\Documents\\!Masters\\shakespeare\\interm";
-    my_File_Management.setInputFileLocation(filepath1);
-    my_File_Management.setIntermediateFileLocation(filepath2);
-    
-    vector<string> inputFiles = my_File_Management.getInputFiles();
-    //*/
 
     // loop though each input file
     for (int i = 0;  i < my_File_Management.getNumberOfInputFiles(); i++)
@@ -102,8 +89,6 @@ int workflow()
         Reduce reduceObj(my_File_Management);
         reduceObj.reduceWrapper();
         //vector<string> Test_Reduce_Input_Vector = my_File_Management.importReduceFile();
-
-
 
         std::cout << "Loop " << i << " complete.\n";
 
